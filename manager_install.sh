87954fedbfaec6f089783b4ae2c3e2f22a0f473f
mandev1 <101856662+mandev1@users.noreply.github.com>
2025-03-29 18:06:01 +0700

    Update manager_install.sh

--- manager_install.sh
+++ manager_install.sh
@@ -32,16 +32,14 @@ while true; do
     echo "======================================"
     echo "1. Update & Upgrade Sistem"
     echo "2. Install Elasticsearch"
-    echo "3. Konfigurasi Elasticsearch"
-    echo "4. Install Kibana"
-    echo "5. Konfigurasi Kibana"
-    echo "6. Generate Token & Kode Verifikasi Kibana"
-    echo "7. Reset Password Elasticsearch"
-    echo "8. Install Fleet Server"
-    echo "9. Restart Service"
+    echo "3. Install Kibana"
+    echo "4. Generate Token & Kode Verifikasi Kibana"
+    echo "5. Reset Password Elasticsearch"
+    echo "6. Install Fleet Server"
+    echo "7. Restart Service"
     echo "0. Keluar"
     echo "======================================"
-    read -p "Pilih opsi [0-9]: " opsi
+    read -p "Pilih opsi [0-7]: " opsi
 
     case $opsi in
         1)
@@ -58,64 +56,40 @@ while true; do
         2)
             echo "Menginstal Elasticsearch..."
             sudo apt install elasticsearch -y
-	    sleep 1
+	    sudo echo "indices.query.bool.max_clause_count: 2000" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+	    sudo echo "http.max_content_length: 400mb" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+	    sudo echo "network.host: $(hostname -I | awk '{print $1}')" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
             sudo systemctl daemon-reload
             sudo systemctl enable elasticsearch.service
             sudo systemctl start elasticsearch.service
-	    sleep 1
-     	    echo "Elasticsearch berhasil diinstal."
-            echo "Selanjutnya Melakukan Konfigurasi Elasticsearch"
-            echo "Pada opsi [3. Konfigurasi Elasticsearch]"
+            echo "Elasticsearch berhasil diinstal."
+            echo "Elasticsearch dapat diakses melalui https://$(hostname -I | awk '{print $1}'):9200"
+            echo "Silahkan Melakukan Reset Password Elasticsearch terlebih dahulu!"
+            echo "Pada opsi [5. Reset Password Elasticsearch]"
             sleep 1
             read -p "Tekan Enter untuk kembali ke menu."
             ;;
         3)
-            echo "Konfigurasi Elasticsearch..."
-            sudo echo "indices.query.bool.max_clause_count: 2000" >> /etc/elasticsearch/elasticsearch.yml
-            sudo echo "http.max_content_length: 400mb" >> /etc/elasticsearch/elasticsearch.yml
-            sudo echo "network.host: $(hostname -I | awk '{print $1}')" >> /etc/elasticsearch/elasticsearch.yml
-	    sleep 1
-            sudo systemctl restart elasticsearch.service
-            echo "Konfigurasi Elasticsearch berhasil."
-            echo "Elasticsearch dapat diakses melalui https://$(hostname -I | awk '{print $1}'):9200"
-            echo "Silahkan Melakukan Reset Password Elasticsearch terlebih dahulu!"
-            echo "Pada opsi [7. Reset Password Elasticsearch]"
-            sleep 1
-            read -p "Tekan Enter untuk kembali ke menu."
-            ;;
-        4)
             echo "Menginstal Kibana..."
             sudo apt install kibana -y
-	    sleep 1
-	    sudo systemctl daemon-reload
+            sudo /usr/share/kibana/bin/kibana-encryption-keys generate >> encrypt.txt
+	    sudo grep "xpack.encryptedSavedObjects.encryptionKey:" encrypt.txt | sudo tee -a /etc/kibana/kibana.yml
+	    sudo grep "xpack.reporting.encryptionKey:" encrypt.txt | sudo tee -a /etc/kibana/kibana.yml
+	    sudo grep "xpack.security.encryptionKey:" encrypt.txt | sudo tee -a /etc/kibana/kibana.yml
+	    sudo echo "xpack.integration_assistant.enabled: false" | sudo tee -a /etc/kibana/kibana.yml
+	    sudo echo "server.host: $(hostname -I | awk '{print $1}')" | sudo tee -a /etc/kibana/kibana.yml
+            sudo systemctl daemon-reload
             sudo systemctl enable kibana.service
             sudo systemctl start kibana.service
-	    sleep 1
-            echo "Kibana berhasil diinstal."
-            echo "Selanjutnya Melakukan Konfigurasi Kibana"
-            echo "Pada opsi [5. Konfigurasi Kibana]"
-            sleep 1
-            read -p "Tekan Enter untuk kembali ke menu."
-            ;;
-        5)
-            echo "Konfigurasi Kibana..."
-            sudo /usr/share/kibana/bin/kibana-encryption-keys generate >> encrypt.txt
-            sudo grep "xpack.encryptedSavedObjects.encryptionKey:" encrypt.txt >> /etc/kibana/kibana.yml
-            sudo grep "xpack.reporting.encryptionKey:" encrypt.txt >> /etc/kibana/kibana.yml
-            sudo grep "xpack.security.encryptionKey:" encrypt.txt >> /etc/kibana/kibana.yml
-            sudo echo "xpack.integration_assistant.enabled: false" >> /etc/kibana/kibana.yml
-            sudo echo "server.host: $(hostname -I | awk '{print $1}')" >> /etc/kibana/kibana.yml
-	    sleep 1
-            sudo systemctl restart kibana.service
             rm encrypt.txt
-            echo "Konfigurasi Kibana berhasil "
+            echo "Kibana berhasil diinstal."
             echo "Kibana dapat diakses melalui https://$(hostname -I | awk '{print $1}'):5601"
             echo "Selanjutnya Masukkan Token & Kode Verifikasi Kibana terlebih dahulu!"
-            echo "Pada opsi [6. Generate Token & Kode Verifikasi Kibana]"
+            echo "Pada opsi [4. Generate Token & Kode Verifikasi Kibana]"
             sleep 1
             read -p "Tekan Enter untuk kembali ke menu."
             ;;
-        6)  
+        4)  
             echo "Silahkan Masukkan Token Kibana ini Pada Web: "
             echo ""
 	    sudo /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana 
@@ -130,13 +104,13 @@ while true; do
 	    sleep 1
 	    read -p "Tekan Enter untuk kembali ke menu."
             ;;
-        7)
+        5)
             echo "Reset password Elasticsearch..."
             sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -i
             sleep 1
             read -p "Tekan Enter untuk kembali ke menu."
             ;;
-        8)
+        6)
             # Instalasi Fleet Server
             echo "Download Installer Fleet Server..."
             curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.17.3-linux-x86_64.tar.gz
@@ -156,7 +130,7 @@ while true; do
 	    sleep 1
 	    read -p "Tekan Enter untuk kembali ke menu."
             ;;
-        9)
+        7)
             echo "Restart Semua Service..."
             sleep 1
             echo "Restart Elasticsearch..."
