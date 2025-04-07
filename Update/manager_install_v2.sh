54c85106a76b7dbe714680388a28b8067aa46e8e
mandev1 <101856662+mandev1@users.noreply.github.com>
2025-04-07 15:55:20 +0700

    Update manager_install_v2.sh

--- Update/manager_install_v2.sh
+++ Update/manager_install_v2.sh
@@ -47,7 +47,7 @@ while true; do
         1)
             echo "Melakukan update & upgrade sistem..."
             sudo apt update && sudo apt upgrade -y
-            sudo apt install apt-transport-https curl gnupg postgresql -y
+            sudo apt install apt-transport-https curl gnupg postgresql postgresql-contrib -y
             sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
             sudo echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
             sudo apt update
